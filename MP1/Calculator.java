7a31aa0632d0ec121ab99b0a4accbffcccf2ae3b
99hhernandez <30508079+99hhernandez@users.noreply.github.com>
2021-09-10 11:28:29 -0500

    Create Calculator.java

---
+++ MP1/Calculator.java
@@ -0,0 +1,19 @@
+public class Calculator {
+	private String name;
+	
+	public Float addition(Float A, Float B){
+		//addition
+	}
+	public Float subtraction(Float A, Float B){
+		//subtraction
+	}
+	public Float multiplication(Float A, Float B){
+		//multiply
+	}
+	public void setname(String N){
+    //set obj name
+  }
+	public String getname(){
+    //get obj name
+  }
+}
