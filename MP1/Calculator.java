d520c85a68abde1b20997f6d7d517f9d06c61320
99hhernandez <30508079+99hhernandez@users.noreply.github.com>
2021-09-10 21:04:39 -0500

    Update Calculator.java

--- MP1/Calculator.java
+++ MP1/Calculator.java
@@ -1,19 +1,19 @@
 public class Calculator {
-	private String name;
+	private String name;                                //object name
 	
-	public Float addition(Float A, Float B){
-		//addition
+	public Float addition(Float A, Float B){            //addition function
+		return A+B;
 	}
-	public Float subtraction(Float A, Float B){
-		//subtraction
+	public Float subtraction(Float A, Float B){         //subtraction function
+		return A-B;
 	}
-	public Float multiplication(Float A, Float B){
-		//multiply
+	public Float multiplication(Float A, Float B){      //multiplication function
+		return A*B;
 	}
-	public void setname(String N){
-    		//set obj name
+	public void setname(String N){                      //set object name
+        this.name = N;
   	}
-	public String getname(){
-    		//get obj name
+	public String getname(){                            //get object name
+        return name;
   	}
 }
