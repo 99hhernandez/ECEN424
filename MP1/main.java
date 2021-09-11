39363617e63b4bcf8ac56c2a7ba52710d81ae200
99hhernandez <30508079+99hhernandez@users.noreply.github.com>
2021-09-10 21:25:02 -0500

    Create main.java

---
+++ MP1/main.java
@@ -0,0 +1,8 @@
+
+
+public class main {
+    public static void main(String[] args) {
+        Calculator mycalc=new Calculator();                 //creating new Calculator object called mycalc
+        mycalc.setname("Group 12");                         //setting mycalc name to Group 12
+    }
+}
