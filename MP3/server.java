18f5ec0a2d5154e00d666ec8eb4d3b18eca728e9
99hhernandez <30508079+99hhernandez@users.noreply.github.com>
2021-12-01 11:31:00 -0600

    Update server.java

--- MP3/server.java
+++ MP3/server.java
@@ -1,18 +1,43 @@
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Vector;
 
-class TCPServer{
-    public static void main(String argv[]) throws Exception{
-        try {
-            String serverMessage;       //server message sent to client upon connection
-            ServerSocket welcomeSocket = new ServerSocket(Integer.parseInt(argv[0]));
+public class server {
+    
+    static int portNumber = 0;
+    public static void main(String[] args) throws IOException{
 
-            while(true) {
-
+        if (args.length != 2) {
+            System.err.println("Usage: java KnockKnockServer <port number>");
+            System.exit(1);
+        }
+ 
+        portNumber = Integer.parseInt(args[0]);
+        ServerSocket serverSocket = new ServerSocket(portNumber);
+        int maxClients = Integer.parseInt(args[1]);
+        int currentClients = 0;
+        Vector<Thread> threads = new Vector<Thread>();
+        while(true){
+            if(currentClients < maxClients){
+                Socket clientSocket = serverSocket.accept();
+                threads.add(new Thread(new ClientHandler(clientSocket)));
+                threads.elementAt(threads.size()-1).start();
+                currentClients += 1;
+            }else if(currentClients == maxClients){
+                for(int i = 0; i < currentClients; i++){
+                    if(threads.elementAt(i).isAlive() == false){
+                        threads.remove(i);
+                        currentClients -= 1;
+                        i--;
+                        System.out.println("Client ended!");
+                    }
+                }
             }
         }
-        catch(NumberFormatException e) {
-            System.out.println("One of the numbers entered was not an integer. Enter an integer and try again.");
-        }
+
     }
 }
